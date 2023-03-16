c5d307ec9b4228003a0de2400b50f2491ed31542
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.pl>
2023-03-16 08:46:13 +0100

    Test

--- src/main/resources/test.js
+++ src/main/resources/test.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 export const options = {
   duration: '1m',
@@ -13,12 +14,11 @@ export const options = {
 
 export default function () {
   const res = http.get('http://localhost:8080/test');
-  sleep(1);
 }
 
 export function handleSummary(data) {
   return {
-    "new2.html": htmlReport(data), // show report in html based format.
+    "summary.html": htmlReport(data), // show report in html based format.
     'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout format...
   };
 }
