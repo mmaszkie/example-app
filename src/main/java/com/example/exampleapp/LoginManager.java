23c76fffa7579b5130166bf12a475c98d8b70d12
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.pl>
2023-03-16 08:08:27 +0100

    Test

--- src/main/java/com/example/exampleapp/LoginManager.java
+++ src/main/java/com/example/exampleapp/LoginManager.java
@@ -1,10 +1,14 @@
 package com.example.exampleapp;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginManager {
 
-    boolean login(String userName, String password) {
-
-
+    @GetMapping("/test")
+    boolean login() {
         return true;
     }
 
